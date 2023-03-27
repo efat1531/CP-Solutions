b2d025da15a2d09816320e3d5c492dc16bfceae8
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:32:33 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1700A | Optimal Path/161178770.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rules
+    testcase(tt, t)
+    {
+        ll n,m;
+        ll ans=0;
+        cin>>n>>m;
+        ll pre=(m*(m+1))/2;
+       //cout<<pre nl;
+        int a=m*2;
+        for(int i=1;i<n;i++)
+        {
+            ans+=a;
+            //cout<<"A -> "<<a nl;
+            a+=m;
+ 
+        }
+        cout<<ans+pre nl;
+ 
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
+ 
