c9d5a969dfa435db421b77adc0205982b3312ca9
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:34:35 +0600

    Committing files

---
+++ CodeForces/GNU C++17/479C | Exams/157441796.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<pair<int,int> > v;
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        v.push_back(make_pair(a,b));
+    }
+    sort(v.begin(),v.end());
+    int day=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(v[i].second>=day)
+           day=v[i].second;
+        else
+            day=v[i].first;
+        //cout<<day nl;
+    }
+    cout<<day nl;
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
