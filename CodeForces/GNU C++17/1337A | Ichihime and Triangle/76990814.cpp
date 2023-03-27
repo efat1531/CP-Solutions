5e570c70d549abdd96558ea29bf295fe6633f62f
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:48:53 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1337A | Ichihime and Triangle/76990814.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(a+b>c)cout<<a<<" "<<b<<" "<<c<<endl;
+        else
+        {
+            a+=b-a;
+            b+=c-a;
+            cout<<a<<" "<<b<<" "<<c<<endl;
+        }
+    }
+}
