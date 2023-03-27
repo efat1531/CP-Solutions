c9cde6715d5e442d1faff495da994910f023cb23
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:47:09 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1391A | Suborrays/89993178.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<string>
+using namespace std;
+#define ll long long int
+#define ull unsigned long long int
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
