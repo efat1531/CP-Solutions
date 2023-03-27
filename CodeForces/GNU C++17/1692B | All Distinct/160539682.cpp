7a9bc2c5a14c992ee9582425db56cd00a631dea3
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:33:06 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1692B | All Distinct/160539682.cpp
@@ -0,0 +1,59 @@
+// Problem: B. All Distinct
+// Contest: Codeforces - Codeforces Round #799 (Div. 4)
+// URL: https://codeforces.com/contest/1692/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rules
+    testcase(tt, t)
+    {
+        int n;
+        cin >> n;
+        map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            cin >> a;
+            mp[a]++;
+        }
+        int dif = n - mp.size();
+        if (dif & 1)
+            cout << mp.size() - 1 nl;
+        else
+            cout << mp.size() nl;
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
