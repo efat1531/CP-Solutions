1fecc938419d03bd296e91816bd1c9fb37c32b0c
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:53:47 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1196A | Three Piles of Candies/73798333.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<string>
+#define ull unsigned long long int
+#define ll long long int
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,c;
+        cin>>a>>b>>c;
+        cout<<(a+b+c)/2<<endl;
+    }
+}
