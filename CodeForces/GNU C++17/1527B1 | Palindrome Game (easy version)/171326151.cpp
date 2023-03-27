458529bc1009661c82620837130e171e38b9752d
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:30:10 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1527B1 | Palindrome Game (easy version)/171326151.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+ 
+void solve()
+{
+    testcase(tt,t)
+    {
+  int n;
+  cin>>n;
+  string s;
+  cin>>s;
+  int num0=0;
+  for(int i=0;i<n;i++)
+   if(s[i]=='0')
+    num0++;
+  if(num0==0)
+   cout<<"DRAW" nl;
+  else if(num0==1 or (num0&1)==0)
+   cout<<"BOB" nl;
+  else 
+  cout<<"ALICE" nl;
+   
+ }
+}
+ 
+int main()
+{
+    fast_io;
+    solve();
+}
