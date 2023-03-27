544ba8648490c557d002bd568924f69b6bfc431d
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:33:30 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1690E | Price Maximization/159915922.cpp
@@ -0,0 +1,69 @@
+// Problem: E. Price Maximization
+// Contest: Codeforces - Codeforces Round #797 (Div. 3)
+// URL: https://codeforces.com/contest/1690/problem/E
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rules
+    testcase(tt, t)
+    {
+        ll n, k;
+        cin >> n >> k;
+        ll ar[n];
+        ll ans = 0;
+        for (int i = 0; i < n; i++)
+            cin >> ar[i], ans += (ar[i] / k);
+        int mod[n];
+        for (int i = 0; i < n; i++)
+            mod[i] = ar[i] % k;
+        for (auto x : mod)
+            cerr << x << " ";
+        cerr nl;
+        sort(mod, mod + n);
+        for (int i = 0, j = n - 1; i < j;)
+        {
+            if (mod[i] + mod[j] >= k)
+            {
+                ans++;
+                i++, j--;
+            }
+            else
+                i++;
+        }
+        cout << ans nl;
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
