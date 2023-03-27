d949bc0331e98f6cfb77e9b6a38db4d93b4870b4
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:32:18 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1699A | The Third Three Number Problem/162762316.cpp
@@ -0,0 +1,51 @@
+// Problem: A. The Third Three Number Problem
+// Contest: Codeforces - Codeforces Round #804 (Div. 2)
+// URL: https://codeforces.com/contest/1699/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rule
+    testcase(tt, t)
+    {
+        int n;
+        cin >> n;
+        if (n & 1)
+            cout << -1 nl;
+        else
+            cout << 0 << " " << n / 2 << " " << n / 2 nl;
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
