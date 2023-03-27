4b9464f8cc6f79006ea555265df068cc40ec8cbc
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:37:45 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1633C | Kill the Monster/144728926.cpp
@@ -0,0 +1,132 @@
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                            And Technology                              || //
+// ||                             Efat Sikder                                || //
+// ||                    Bismillah-ir-Rahman-ir-Rahim                        || //
+// ||                                                                        || //
+// ============================================================================ //
+// ======================================================================================================================= //
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// ======================================================================================================================= //
+ 
+typedef unsigned long long            ull;
+typedef bool                          flag;
+const int                             N = 1e6 + 5;
+// ============================================Define Macros============================================================== //
+ 
+#define int                           long long int
+#define double                        long double
+#define pb                       push_back
+#define ff                            first
+#define ss                            second
+#define pp                            pop_back
+#define mp                      make_pair
+#define vin                     vector<int>
+#define vch                           vector<char>
+#define vst                           vector<string>
+#define pii                         pair<int,int>
+#define triplet                       pair<int,pair<int,int>>
+#define mpin                          map<int,int>
+#define nl                            <<"\n"
+#define mod                           1000000007
+#define pi                            2 * acos (0.0)
+ 
+// =======================================Define Statements=============================================================== //
+ 
+#define print(x)                   printf("%lld\n",x)
+#define input(x)                      scanf("%lld",&x)
+#define stringin(x)                   scanf(" %[^\n]%*c", x)
+#define sz(x)                         (x).size()
+#define read(x)                       int x; cin >> x
+#define fr(a,b)                       for(int i = a; i < b; i++)
+#define mm(x,v)                       memset((x),v,sizeof(x));
+#define testcase(t,x)                 int t; cin>>t; for(int x=1 ; x<=t ; x++)
+#define all(x)                        (x).begin(), (x).end()
+#define erase(a,b)                    a.erase(a.begin()+(b-1))
+#define prDouble(x,y)                 fixed << setprecision(y) << x
+#define lcm(a,b)                      (a*b)/__gcd(a,b);
+#define sortof(x)                     sort((x).begin(),(x).end());
+#define fast_io                       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+// ======================================Debugger Code for IDE============================================================ //
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr nl;
+#else
+#define debug(x)
+#endif
+#define dbn cerr nl;
+ 
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+// ===========================================Main Code Goes Here========================================================== //
+ 
+void solve() {
+ //My code My Rules
+ testcase(tt, t)
+ {
+  int ch, ca;
+  cin >> ch >> ca;
+  int dh, da;
+  cin >> dh >> da;
+  int tc, pca, pch;
+  cin >> tc >> pca >> pch;
+  flag f = false;
+  for (int i = tc; i >= 0; i--)
+  {
+   int attack_increase = (i * pca) + ca;
+   debug(attack_increase)
+   int deffence_increase = ((tc - i) * pch) + ch;
+   debug(deffence_increase)
+   int tempattack = (dh + (attack_increase - 1)) / attack_increase;
+   int tempdeffence = (deffence_increase + (da - 1)) / da;
+   debug(tempattack)
+   debug(tempdeffence)
+   if (tempattack <= tempdeffence)
+   {
+    f = true;
+    break;
+   }
+ 
+  }
+  if (f == true)
+   cout << "YES" nl;
+  else
+   cout << "NO" nl;
+  dbn
+ }
+}
+// ===========================================Sublime IDE CODE============================================================ //
+ 
+void init_code() {
+ fast_io;
+#ifndef ONLINE_JUDGE
+ freopen("Error.txt", "w", stderr);
+#endif
+}
+ 
+int32_t main() {
+ init_code();
+ solve();
+ return 0;
+}
