4ac6e955db39761cebce4046b5070ace9927f149
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:35:58 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1665A | GCD vs LCM/153022978.cpp
@@ -0,0 +1,48 @@
+// Problem: A. GCD vs LCM
+// Contest: Codeforces - Codeforces Round #781 (Div. 2)
+// URL: https://codeforces.com/contest/1665/problem/0
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rules
+    testcase(tt, t)
+    {
+        ll n;
+        cin >> n;
+        cout << n - 3 << " 1 1 1" nl;
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
