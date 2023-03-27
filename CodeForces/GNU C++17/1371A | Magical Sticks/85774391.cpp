e118b059cd3724d7ab542254fb6d25e546f3c54e
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:48:04 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1371A | Magical Sticks/85774391.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<string>
+using namespace std;
+#define ll long long int
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<(n+1)/2<<endl;
+ 
+    }
+}
