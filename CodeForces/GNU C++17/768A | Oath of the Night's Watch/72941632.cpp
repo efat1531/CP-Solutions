e6a8df85828afcf0eb4df20c64b27c591fd977cc
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:58:34 +0600

    Committing files

---
+++ CodeForces/GNU C++17/768A | Oath of the Night's Watch/72941632.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<string>
+#define ull unsigned long long int
+#define ll long long int
+using namespace std;
+int main()
+{
+    int n,c=0;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    for(int i=0;i<n;i++)if(a[i]>a[0]&&a[n-1]>a[i])c++;
+    cout<<c;
+ 
+}
