013c7450be4ad459434f3e2c03d00e32977ca2ab
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:57:56 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1325A | EhAb AnD gCd/73239912.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<string>
+#define ull unsigned long long int
+#define ll long long int
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll x;
+        cin>>x;
+        cout<<"1"<<" "<<x-1<<endl;
+    }
+}
