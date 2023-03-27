495cc75dedc2a4c2a1d97d06a2daa7f1224e96ae
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:36:11 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1660A | Vasya and Coins/151533759.cpp
@@ -0,0 +1,61 @@
+// Problem: A. Vasya and Coins
+// Contest: Codeforces - Codeforces Round #780 (Div. 3)
+// URL: https://codeforces.com/contest/1660/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rules
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b;
+        cin >> a >> b;
+        ll ans = 1;
+        if (a == 0)
+        {
+            cout << 1 nl;
+        }
+        else
+        {
+            ans += a + (b * 2);
+            cout << ans nl;
+        }
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
