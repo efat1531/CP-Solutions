b19294470a6cb424b4e21f95bdd774cb0950cc9b
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:27:07 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1772B | Matrix Rotation/185821286.cpp
@@ -0,0 +1,56 @@
+// Author - Sp3cTer
+// Date   - 18 - 12 - 2022
+// Time   - 20 - 35
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi 2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                \
+  int x;                                                                       \
+  cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b) (a * b) / __gcd(a, b);
+#define sortof(x) sort((x).begin(), (x).end());
+#define fast_io                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                         \
+  int t;                                                                       \
+  cin >> t;                                                                    \
+  for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve() {
+    testcase(tt,t)
+    {
+ int a,b,c,d;
+ cin>>a>>b>>c>>d;
+ int mn=min(a,min(b,min(c,d)));
+ int mx=max(a,max(b,max(c,d)));
+ string ans="NO";
+ if(a==mn and d==mx)
+ {
+     ans="YES";
+ }else if(a==mx and d==mn)
+ {
+     ans="YES";
+ }else if(b==mn and c==mx)
+     ans="YES";
+ else if(b==mx and c==mn)
+     ans="YES";
+ cout<<ans nl;
+    }
+}
+ 
+int32_t main() {
+  fast_io;
+  solve();
+  return 0;
+}
