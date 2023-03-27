528170decfe5a04af84f0c5fc6de1b08d985ae50
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 15:03:35 +0600

    Committing files

---
+++ CodeForces/GNU C++17/867A | Between the Offices/71832124.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    string a;
+    cin>>a;
+    if(a[0]=='S'&&a[n-1]=='F')
+    {
+        cout<<"YES";
+    }else cout<<"NO";
+}
