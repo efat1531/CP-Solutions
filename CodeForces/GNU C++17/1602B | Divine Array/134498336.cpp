567fca1b83e8f3f25ac23bacdb26428e97011841
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:41:20 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1602B | Divine Array/134498336.cpp
@@ -0,0 +1,145 @@
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                            And Technology                              || //
+// ||                             Efat Sikder                                || //
+// ||                    Bismillah-ir-Rahman-ir-Rahim                        || //
+// ||                                                                        || //
+// ============================================================================ //
+// ======================================================================================================================= //
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// ======================================================================================================================= //
+ 
+typedef unsigned long long            ull;
+typedef long double                   ld;
+ 
+// ============================================Define Macros============================================================== //
+ 
+#define int                           long long int
+#define pb                       push_back
+#define ff                            first
+#define ss                            second
+#define pp                            pop_back
+#define mp                      make_pair
+#define vin                     vector<int>
+#define vch                           vector<char>
+#define vst                           vector<string>
+#define pii                         pair<int,int>
+#define triplet                       pair<int,pair<int,int>>
+#define mpin                          map<int,int>
+#define mod                           1000000007
+#define pi                            2 * acos (0.0)
+#define mm(x,v)                       memset((x),v,sizeof(x));
+ 
+// =======================================Define Statements=============================================================== //
+ 
+#define print(x)                   printf("%lld\n",x)
+#define input(x)                      scanf("%lld",&x)
+#define stringin(x)                   scanf(" %[^\n]%*c", x)
+#define sz(x)                         (x).size()
+#define read(x)                       int x; cin >> x;
+#define fr(a,b)                       for(int i = a; i < b; i++)
+#define testcase(t,x)                 int t; cin>>t; for(int x=1 ; x<=t ; x++)
+#define all(x)                        (x).begin(), (x).end()
+#define erase(a,b)                    a.erase(a.begin()+(b-1))
+#define prDouble(x,y)                 fixed << setprecision(y) << x<<endl
+#define lcm(a,b)                      (a*b)/__gcd(a,b);
+#define sortof(x)                     sort((x).begin(),(x).end());
+#define fast_io                       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+// ======================================Debugger Code for IDE============================================================ //
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+#define dbn cerr<<endl;
+ 
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+// ==========================================Main Code Goes Here========================================================== //
+ 
+void solve() {
+ testcase(tt, t)
+ {
+  int n;
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++)
+   cin >> arr[i];
+  int ar2[n + 1][n];
+  for (int i = 0; i < n; i++)
+  {
+   ar2[0][i] = arr[i];
+  }
+  for (int i = 1; i <= n; i++)
+  {
+   map<int, int> m;
+   for (int i = 0; i < n; i++)
+   {
+    m[arr[i]]++;
+   }
+   debug(m)
+   dbn
+ 
+   for (int j = 0; j < n; j++)
+   {
+    arr[j] = m[arr[j]];
+    ar2[i][j] = arr[j];
+   }
+ 
+  }
+  int q;
+  cin >> q;
+  while (q--)
+  {
+   int x, k;
+   cin >> x >> k;
+   x--;
+   if (k <= n)
+   {
+    cout << ar2[k][x] << endl;
+   }
+   else
+   {
+    cout << ar2[n][x] << endl;
+   }
+  }
+ 
+ }
+}
+ 
+// ===========================================Sublime IDE CODE============================================================ //
+ 
+void init_code() {
+ fast_io;
+#ifndef ONLINE_JUDGE
+ freopen("Error.txt", "w", stderr);
+#endif
+}
+ 
+int32_t main() {
+ init_code();
+ solve();
+ return 0;
+}
