bbb0e4bac05199f12a8e0a26c46d5ed5c8cdb538
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:23:49 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1807A | Plus or Minus/198113189.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi 2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                \
+  int x;                                                                       \
+  cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b) (a * b) / __gcd(a, b);
+#define sortof(x) sort((x).begin(), (x).end());
+#define fast_io                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                         \
+  int t;                                                                       \
+  cin >> t;                                                                    \
+  for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve() {
+    testcase(tt,t)
+    {
+ int a,b,c;
+ cin>>a>>b>>c;
+ if(a+b==c)
+     cout<<'+' nl;
+ else
+     cout<<'-' nl;
+    }
+}
+ 
+int32_t main() {
+  fast_io;
+  solve();
+  return 0;
+}
