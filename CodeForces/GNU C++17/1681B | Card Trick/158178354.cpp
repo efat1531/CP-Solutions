1006e79b03390ee060bb7726acf06019bb35ab66
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:34:21 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1681B | Card Trick/158178354.cpp
@@ -0,0 +1,68 @@
+// Problem: B. Card Trick
+// Contest: Codeforces - Educational Codeforces Round 129 (Rated for Div. 2)
+// URL: https://codeforces.com/contest/1681/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rules
+    testcase(tt, t)
+    {
+        int n;
+        cin >> n;
+        int ar[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> ar[i];
+        }
+        int q;
+        int p = 0;
+        cin >> q;
+        while (q--)
+        {
+            int a;
+            cin >> a;
+            if (a + p >= n)
+                p = (a + p) - n;
+            else
+                p += a;
+            // cerr << "P->" << p nl;
+            // cerr << ar[p] nl;
+        }
+        // cerr nl nl;
+        cout << ar[p] nl;
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
