c1213bc62a1f8894a1d59787d9f4d10466809632
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:28:12 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1748B | Diverse Substrings/181423815.cpp
@@ -0,0 +1,60 @@
+// Author - Sp3cTer
+// Date   - 18 - 11 - 2022
+// Time   - 20 - 00
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi 2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                \
+  int x;                                                                       \
+  cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b) (a * b) / __gcd(a, b);
+#define sortof(x) sort((x).begin(), (x).end());
+#define fast_io                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                         \
+  int t;                                                                       \
+  cin >> t;                                                                    \
+  for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve() {
+    testcase(tt,t)
+    {
+ int n;
+ cin>>n;
+ string s;
+ cin>>s;
+ ll ans=0;
+ for(int i=0;i<n;i++)
+ {
+     map<char,int>mp;
+     int mx=0;
+     for(int j=i;j<min(i+101,n);j++)
+     {
+  mp[s[j]]++;
+  mx=max(mx,mp[s[j]]);
+  if(mp.size()>=mx)
+  {
+      ans++;
+  }
+     }
+ }
+ cout<<ans nl;
+    }
+}
+ 
+int32_t main() {
+  fast_io;
+  solve();
+  return 0;
+}
