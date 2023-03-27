0e6a89b5f8f842c4ce484c89c3594c25de2df70d
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:46:45 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1450A | Avoid Trygub/100870047.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        sort(s.begin(),s.end());
+        cout<<s<<endl;
+    }
+}
