3f011f5223247655bdb400671bd8be6aba8b63de
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:21:50 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc294_a/39903194.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi 2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                \
+  int x;                                                                       \
+  cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b) (a * b) / __gcd(a, b);
+#define sortof(x) sort((x).begin(), (x).end());
+#define fast_io                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                         \
+  int t;                                                                       \
+  cin >> t;                                                                    \
+  for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+	int a;
+	cin>>a;
+	if(a&1)
+	    continue;
+	else
+	    cout<<a<<" ";
+    }
+}
+ 
+int32_t main() {
+  fast_io;
+  solve();
+  return 0;
+}
