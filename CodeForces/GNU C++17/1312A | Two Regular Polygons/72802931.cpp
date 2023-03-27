acf9b9b7e9bf7156a96103dcc91df215283b2f7b
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:59:12 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1312A | Two Regular Polygons/72802931.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<string>
+#define ull unsigned long long int
+#define ll long long int
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m;
+        cin>>n>>m;
+        if(n%m==0)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
+ 
