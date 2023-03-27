95b70e54a1a0285e78c42831985729e7029dbb48
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:31:59 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1701A | Grass Field/163217644.cpp
@@ -0,0 +1,53 @@
+// Problem: A. Grass Field
+// Contest: Codeforces - Educational Codeforces Round 131 (Rated for Div. 2)
+// URL: https://codeforces.com/contest/1701/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rule
+    testcase(tt, t)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        if (a + b + c + d == 0)
+            cout << 0 nl;
+        else if (a + b + c + d == 4)
+            cout << 2 nl;
+        else
+            cout << 1 nl;
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
