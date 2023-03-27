ac0bf49c979066ffa5a52873fb94229bfb792625
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:58:40 +0600

    Committing files

---
+++ CodeForces/GNU C++17/160A | Twins/72939847.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<cmath>
+#include<string>
+#define ull unsigned long long int
+#define ll long long int
+using namespace std;
+int main()
+{
+    int n,sum1=0,sum2=0,c=0;
+    cin>>n;
+    int a[n];
+    int j=n-1;
+    for(int i=0;i<n;i++)cin>>a[i],sum1+=a[i];
+    sort(a,a+n);
+    while(sum2<=sum1/2)
+    {
+        sum2+=a[j];
+        //sum1-=a[j];
+        j--;
+        c++;
+    }
+    cout<<c;
+}
