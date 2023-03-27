617787b5289d0097c0f1528fd5b61fd33ec32626
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:39:59 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1619C | Wrong Addition/140408210.cpp
@@ -0,0 +1,152 @@
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                            And Technology                              || //
+// ||                             Efat Sikder                                || //
+// ||                    Bismillah-ir-Rahman-ir-Rahim                        || //
+// ||                                                                        || //
+// ============================================================================ //
+// ======================================================================================================================= //
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// ======================================================================================================================= //
+ 
+typedef unsigned long long            ull;
+typedef long double                   ld;
+typedef bool                          flag;
+ 
+// ============================================Define Macros============================================================== //
+ 
+#define int                           long long int
+#define pb                       push_back
+#define ff                            first
+#define ss                            second
+#define pp                            pop_back
+#define mp                      make_pair
+#define vin                     vector<int>
+#define vch                           vector<char>
+#define vst                           vector<string>
+#define pii                         pair<int,int>
+#define triplet                       pair<int,pair<int,int>>
+#define mpin                          map<int,int>
+#define nl                            <<"\n"
+#define mod                           1000000007
+#define pi                            2 * acos (0.0)
+#define mm(x,v)                       memset((x),v,sizeof(x));
+ 
+// =======================================Define Statements=============================================================== //
+ 
+#define print(x)                   printf("%lld\n",x)
+#define input(x)                      scanf("%lld",&x)
+#define stringin(x)                   scanf(" %[^\n]%*c", x)
+#define sz(x)                         (x).size()
+#define read(x)                       int x; cin >> x
+#define fr(a,b)                       for(int i = a; i < b; i++)
+#define testcase(t,x)                 int t; cin>>t; for(int x=1 ; x<=t ; x++)
+#define all(x)                        (x).begin(), (x).end()
+#define erase(a,b)                    a.erase(a.begin()+(b-1))
+#define prDouble(x,y)                 fixed << setprecision(y) << x<<endl
+#define lcm(a,b)                      (a*b)/__gcd(a,b);
+#define sortof(x)                     sort((x).begin(),(x).end());
+#define fast_io                       ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ 
+// ======================================Debugger Code for IDE============================================================ //
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr nl;
+#else
+#define debug(x)
+#endif
+#define dbn cerr nl;
+ 
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+void _print(bool t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+// ==========================================Main Code Goes Here========================================================== //
+ 
+void solve() {
+ //My code My Rules
+ testcase(t, tt)
+ {
+  string a, s;
+  string b = "";
+  cin >> a >> s;
+  flag f = false;
+  int sp = s.size() - 1, ap = a.size() - 1;
+  while (sp >= ap and ap >= 0)
+  {
+   if (s[sp] - '0' >= a[ap] - '0')
+   {
+    int num = s[sp] - a[ap];
+    char c = '0' + num;
+    sp--; ap--;
+    b += c;
+   }
+   else if (sp >= 1)
+   {
+    if (s[sp - 1] != '1')
+    {
+     f = true;
+     break;
+    }
+    int num = ((s[sp - 1] - '0') * 10) + (s[sp] - '0');
+    char c = '0' + (num - (a[ap] - '0'));
+    sp -= 2, ap--;
+    b += c;
+   } else
+   {
+    f = true;
+    break;
+   }
+  }
+  debug(ap) debug(sp) dbn
+  for (int i = sp; i >= 0; i--)
+   b += s[i];
+  if (f == true or ap > -1)
+   cout << -1 nl;
+  else
+  {
+   flag f1 = false;
+   for (int i = b.size() - 1; i >= 0; i--)
+   {
+    if (b[i] != '0' and f1 == false)
+     f1 = true, cout << b[i];
+    else if (f1 == true)cout << b[i];
+   }
+   cout nl;
+  }
+ }
+}
+ 
+// ===========================================Sublime IDE CODE============================================================ //
+ 
+void init_code() {
+ fast_io;
+#ifndef ONLINE_JUDGE
+ freopen("Error.txt", "w", stderr);
+#endif
+}
+ 
+int32_t main() {
+ init_code();
+ solve();
+ return 0;
+}
