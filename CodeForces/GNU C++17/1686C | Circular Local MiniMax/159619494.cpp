38a2fd66f73775682993ffe4098834dd937bae31
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:33:47 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1686C | Circular Local MiniMax/159619494.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long int ll;
+typedef unsigned long long l1;
+typedef double db;
+#define pi    2 * acos(0.0)
+#define sz(x) (x).size()
+#define read(x)                                                                                                        \
+    int x;                                                                                                             \
+    cin >> x
+#define prDouble(x, y) fixed << setprecision(y) << x
+#define lcm(a, b)      (a * b) / __gcd(a, b);
+#define sortof(x)      sort((x).begin(), (x).end());
+#define fast_io                                                                                                        \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL);                                                                                                     \
+    cout.tie(NULL)
+#define mm(x, v) memset((x), v, sizeof(x));
+#define testcase(t, x)                                                                                                 \
+    int t;                                                                                                             \
+    cin >> t;                                                                                                          \
+    for (int x = 1; x <= t; x++)
+#define nl << "\n"
+ 
+void solve()
+{
+    // My code My Rules
+    testcase(tt, t)
+    {
+       int n;
+       cin>>n;
+       int ar[n];
+       for(int i=0;i<n;i++)
+            cin>>ar[i];
+        sort(ar,ar+n);
+       if(n&1)
+        cout<<"NO" nl;
+       else
+       {
+           int br[n];
+           string ans="YES";
+           for(int i=0,j=n/2,k=0;i<n/2;i++,j++,k+=2)
+           {
+ 
+               br[k]=ar[i];
+               br[k+1]=ar[j];
+           }
+           for(int i=0;i<n-1;i++)
+           {
+               if(br[i]==br[i+1])
+               {
+                   ans="NO";
+                   break;
+               }
+           }
+ 
+           if(ans=="YES")
+           {
+               cout<< ans nl;
+               for(auto x:br)
+                cout<<x<<" ";
+               cout nl;
+           }else
+            cout<< ans nl;
+ 
+       }
+    }
+}
+ 
+int32_t main()
+{
+    fast_io;
+    solve();
+    return 0;
+}
+ 
