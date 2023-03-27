7f18d57e0f5dddf70a073b1d3f382ad35b5f9f17
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:41:52 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1607A | Linear Keyboard/134077883.cpp
@@ -0,0 +1,98 @@
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                            And Technology                              || //
+// ||                             Efat Sikder                                || //
+// ||                    Bismillah-ir-Rahman-ir-Rahim                        || //
+// ||                                                                        || //
+// ============================================================================ //
+#include<bits/stdc++.h>
+ 
+#define int                     long long int
+#define ld                      long double
+#define ull                     unsigned long long
+ 
+#define pb                       push_back
+#define ff                            first
+#define ss                            second
+#define pp                            pop_back
+#define mp                      make_pair
+#define vin                     vector<int>
+#define vch                           vector<char>
+#define vst                           vector<string>
+#define pii                         pair<int,int>
+#define triplet                       pair<ll,pair<ll,ll>>
+#define mpin                          map<int,int>
+#define mod                           1000000007
+#define pi                            2 * acos (0.0)
+ 
+#define print(x)                   printf("%lld\n",x)
+#define input(x)                      scanf("%lld",&x)
+#define stringin(x)                   scanf(" %[^\n]%*c", x)
+#define sz(x)                         (x).size()
+#define read(x)                       int x; cin >> x;
+#define loj(case_no,result)           cout<<"Case "<<case_no<<": "<<result<<endl
+#define loj2(case_no,result)        cout<<"Case "<<case_no<<":"<<endl<<result<<endl
+#define fr(a,b)                       for(int i = a; i < b; i++)
+#define frc(a,b)                      for(int i = a; i > b; i--)
+#define rep(i,a,b)                    for(int i = a; i < b; i++)
+#define testcase(t,x)                 int t; cin>>t; for(int x=1 ; x<=t ; x++)
+#define test(t)                       int t; cin>>t; while(t--)
+#define all(x)                        (x).begin(), (x).end()
+#define setbits(x)                    __builtin_popcountll(x)
+#define zrobits(x)                    __builtin_ctzll(x)
+#define prDouble(x,y)                 fixed << setprecision(y) << x<<endl
+#define lcm(a,b)                      (a*b)/__gcd(a,b);
+#define erase(a,b)                    a.erase(a.begin()+(b-1))
+#define sortof(x)                     sort((x).begin(),(x).end());
+#define printvec(x,n)                 for(int i=0;i<(n);i++)cout<<(x)[i]<<" ";cout<<endl
+ 
+#define db(x)                         cout<<#x <<"=" <<x<<endl
+#define time                          cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#define starttime                     auto t1 = chrono::high_resolution_clock::now();
+#define endtime                       auto t2 = chrono::high_resolution_clock::now(); std::chrono::duration<double, milli> elapsed = t2 - t1;cerr << "Program Runtime is: " << elapsed.count() << " milliseconds."<<endl;
+ 
+#define fast_io                       ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+ 
+void solve()
+{
+ test(t)
+ {
+  string a, b;
+  cin >> a >> b;
+  int ar[30];
+  for (int i = 0; i < sz(a); i++)
+  {
+   int pos = a[i] - 'a';
+   //db(pos);
+   ar[pos] = i + 1;
+  }
+  int sum = 0;
+  fr(1, sz(b))
+  {
+   int d = b[i] - 'a';
+   int e = b[i - 1] - 'a';
+   sum += abs(ar[d] - ar[e]);
+  }
+  cout << sum << endl;
+ }
+ 
+}
+ 
+void init_code() {
+ fast_io;
+#ifndef ONLINE_JUDGE
+ freopen("input.txt", "r", stdin);
+ freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+int32_t main()
+{
+ starttime;
+ init_code();
+ solve();
+ endtime;
+ return 0;
+}
