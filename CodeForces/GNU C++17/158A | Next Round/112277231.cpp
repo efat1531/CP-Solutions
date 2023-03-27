0317539813c1f8a6e0862098c6fff49969fbc16b
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:46:23 +0600

    Committing files

---
+++ CodeForces/GNU C++17/158A | Next Round/112277231.cpp
@@ -0,0 +1,84 @@
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business,Agriculture           || //
+// ||                           And Technology                               || //
+// ||                             Efat Sikder                                || //
+// ||                    Bismillah-ir-Rahman-ir-Rahim                        || //
+// ||                                                                        || //
+// ============================================================================ //
+#include<bits/stdc++.h>
+ 
+#define int                     long long
+#define ld                      long double
+#define ull                     unsigned long long int
+ 
+#define pb                       push_back
+#define pp                            pop_back
+#define mp                      make_pair
+#define vin                     vector<int>
+#define vch                           vector<char>
+#define vst                           vector<string>
+#define pii                         pair<int,int>
+#define triplet                       pair<ll,pair<ll,ll>>
+#define mpin                          map<int,int>
+#define mod                           1000000007
+#define pi                            2 * acos (0.0)
+ 
+#define print(x)                   printf("%lld\n",x)
+#define input(x)                      scanf("%lld",&x)
+#define stringin(x)                   scanf(" %[^\n]%*c", x)
+#define sz(x)                         (x).size()
+#define read(x)                       int x; cin >> x;
+#define loj(case_no,result)           cout<<"Case "<<case_no<<": "<<result<<endl
+#define loj2(case_no,result)        cout<<"Case "<<case_no<<":"<<endl<<result<<endl
+#define fr(a,b)                       for(int i = a; i < b; i++)
+#define frc(a,b)                      for(int i = a; i > b; i--)
+#define rep(i,a,b)                    for(int i = a; i < b; i++)
+#define testcase(t)                  int t ; cin>>t ; while(t--)
+#define all(x)                        (x).begin(), (x).end()
+#define setbits(x)                    __builtin_popcountll(x)
+#define zrobits(x)                    __builtin_ctzll(x)
+#define prDouble(x)                   cout << fixed << setprecision(10) << x<<endl
+#define lcm(a,b)                      (a*b)/__gcd(a,b);
+#define erase(a,b)                    a.erase(a.begin()+(b-1))
+#define sortof(x)                     sort((x).begin(),(x).end());
+ 
+#define db(x)                         cout<<#x <<"=" <<x<<endl
+#define time                          cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#define fast_io                       ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+ 
+void solve()
+{
+ read(n);
+ read(k);
+ vin v;
+ fr(0, n)
+ {
+  read(a);
+  v.pb(a);
+ }
+ int c = v[k - 1];
+ int d = 0;
+ sortof(v);
+ fr(0, n)
+ {
+  if (v[i] >= c && v[i] != 0)d++;
+ }
+ print(d);
+}
+void init_code() {
+ fast_io;
+#ifndef ONLINE_JUDGE
+ freopen("input.txt", "r", stdin);
+ freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+int32_t main()
+{
+ init_code();
+ solve();
+ time;
+ return 0;
+}
