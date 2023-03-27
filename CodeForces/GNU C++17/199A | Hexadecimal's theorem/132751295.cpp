5af8f5c3c0aeaaeed8c6f074ff849dd02e25e75a
efat1531 <61145086+efat1531@users.noreply.github.com>
2023-03-27 14:42:52 +0600

    Committing files

---
+++ CodeForces/GNU C++17/199A | Hexadecimal's theorem/132751295.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define db(x) cout<< #x <<" = "<< x<<endl
+#define test(t) int t;cin>>t;while(t--)
+#define read(x) int x; cin>>x
+#define sz(s) s.size()
+#define sortof(x) sort( s.begin(),s.end())
+#define ff first
+#define ss second
+using  namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"0 0 "<<n<<endl;
+}
